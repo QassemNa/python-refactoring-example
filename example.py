4e729930e163ce375103bc00a35b516634c20bd0
QassemNa <alnasserqassem@gmail.com>
2021-02-28 22:10:33 +0300

    refactored

--- example.py
+++ example.py
@@ -1,7 +1,11 @@
 def calculate_print(a,b):
     sum = a+b
     multi = a*b
+    return sum, multi
+
+def printing(sum, multi):
     print('Sum is', sum)
     print('Multi is', multi)
 
-calculate_print(1,2)
+sum, multi = calculate_print(1,2)
+printing(sum, multi)
