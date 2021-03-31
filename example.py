f2fb984045f32698400e5e39d48e2ba591c8e263
QassemNa <alnasserqassem@gmail.com>
2021-03-31 06:47:10 +0300

    changed two files

--- example.py
+++ example.py
@@ -1,11 +1,11 @@
-def calculate_print1(a,b):
+def calculate_print0(a,b):
     sum = a+b
     multi = a*b
     return sum, multi
 
-def printing1(sum, multi):
+def printing0(sum, multi):
     print('Sum is', sum)
     print('Multi is', multi)
 
-sum, multi = calculate_print1(1,2)
-printing1(sum, multi)
+sum, multi = calculate_print0(1,2)
+printing0(sum, multi)
--- try.py
+++ try.py
@@ -1,7 +1,4 @@
 class PizzaDelivery:
     # ...
     def getRating(self):
-        return 2 if self.moreThanFiveLateDeliveries() else 1
-
-    def moreThanFiveLateDeliveries(self):
-        return self.numberOfLateDeliveries > 5
+        return 2 if self.numberOfLateDeliveries > 5 else 1
