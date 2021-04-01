e7b90648dc4afda3c0d7247961dd29993969ae71
Qassem Alnasser <47501990+QassemNa@users.noreply.github.com>
2021-04-01 13:56:21 +0300

    Update exctract.py

--- exctract.py
+++ exctract.py
@@ -1,6 +1,7 @@
 def printOwing1(a,b):
     # print details
     print("name:", a)
-    print_amount1(b)
-def print_amount1(b):
+    print_amount2(b)
+def print_amount2(b):
     print("amount:",b)
+printOwing1("MyName", 50)
