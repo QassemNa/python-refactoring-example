72ed8cecf3ee3f130d1f26e2a0b1869567fb11c2
Qassem Alnasser <47501990+QassemNa@users.noreply.github.com>
2021-04-03 15:34:06 +0300

    Update example.py

--- example.py
+++ example.py
@@ -1,11 +1,9 @@
-def calculate_print0(a,b):
-    sum = a+b
-    multi = a*b
-    return sum, multi
+class Test:
+    def calculate_print0(a,b):
+        sum = a+b
+        multi = a*b
+        return sum, multi
 
-def printing0(sum, multi):
-    print('Sum is', sum)
-    print('Multi is', multi)
-
-sum, multi = calculate_print0(1,2)
-printing0(sum, multi)
+    def printing0(sum, multi):
+        print('Sum is', sum)
+        print('Multi is', multi)
