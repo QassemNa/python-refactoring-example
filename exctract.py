c89e23b91661771b973f060d970676b265bd65ac
QassemNa <alnasserqassem@gmail.com>
2021-04-12 16:34:17 +0300

    new folder,extract and inline

--- exctract.py
+++ exctract.py
@@ -1,5 +1,7 @@
 def printOwing1(a,b):
     # print details
     print("name:", a)
+    amount(b)
+def amount(b):
     print("amount:",b)
 printOwing1("MyName", 50)
--- folder/inline.py
+++ folder/inline.py
@@ -1,8 +1,5 @@
 def calculate(a,b):
     c = a + b
-    fprint(c)
-
-def fprint(c):
     print("The answer is,", c)
 
 calculate(1,2)
